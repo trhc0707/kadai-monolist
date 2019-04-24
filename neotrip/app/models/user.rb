3872187e4360c7076862d6851f2b214a9f45a33b
takuma.imamura <trhc0707@icloud.com>
2019-04-24 15:55:21 +0000

    user register

---
+++ neotrip/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+                    
+  has_secure_password
+
+end
--- neotrip/config/initializers/generators.rb
+++ neotrip/config/initializers/generators.rb
@@ -1,6 +1,6 @@
 Rails.application.config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    g.skip routes true
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
 end
